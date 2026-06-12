fc149f8924cb2a52b762936f8de2f4106becf695
the13thgeek <aldreich.aguilar@yahoo.com>
2026-06-11 20:13:42 -0400

    Info update

--- src/components/UpdateText.jsx
+++ src/components/UpdateText.jsx
@@ -11,6 +11,7 @@ const UpdateText = () => {
             <h3>In Active Development</h3>
             <p>This page is currently being updated for the <b>Power Morphicon 2026</b>. Stay tuned for new information coming soon!</p>
             <ul>
+              <li>June 11, 2026 - Added new <b>Guests</b> information</li>
               <li>June 4, 2026 - Added new <b>Guests</b> information</li>
               <li>June 3, 2026 - Added first wave of <b>Guests</b> information</li>
             </ul>
