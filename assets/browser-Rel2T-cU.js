a5c96aa890385bd99435c3b9e9f72facd9282224
the13thgeek <aldreich.aguilar@yahoo.com>
2026-06-19 20:22:34 -0400

    Updates

--- assets/browser-Rel2T-cU.js
+++ assets/browser-Rel2T-cU.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-DzzIT6p_.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-CxOTfjZJ.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
