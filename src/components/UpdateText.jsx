f4468904d266c2a959f240b2931d8cfeff4cb40c
the13thgeek <aldreich.aguilar@yahoo.com>
2026-08-25 21:18:27 -0400

    PMC 2026 update

--- src/components/UpdateText.jsx
+++ src/components/UpdateText.jsx
@@ -11,9 +11,9 @@ const UpdateText = () => {
             <h3>Latest Updates</h3>
             {/* <p>This page is currently being updated for the <b>Power Morphicon 2026</b>. Stay tuned for new information coming soon!</p> */}
             <ul>
+              <li>August 25, 2026 - Added <b>Guests</b> and <b>Panels</b> information</li>
               <li>August 23, 2026 - Added <b>Panels</b> information</li>
               <li>August 22, 2026 - Updated <b>Photo Ops</b> information</li>
-              <li>August 19, 2026 - Added <b>Guests</b> information; minor UI fixes</li>
             </ul>
         </div>
     </Tile>
