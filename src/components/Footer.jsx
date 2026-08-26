b16fc6c78d74d78060350241779d9a525a8ecf7e
the13thgeek <aldreich.aguilar@yahoo.com>
2026-08-25 21:25:59 -0400

    PMC 2026 update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer>
-        <p><b>Disclaimer:</b> The data displayed on this page is sourced directly from the <b>Official Power Morphicon website</b>. We do not verify the accuracy of the information provided by PMC. Therefore, this service will not responsible for any errors, inaccuracies, or discrepancies in the data. The information is provided "as is," and we recommend verifying details directly with PMC for the most accurate and up-to-date information.</p>
+        <p><b>Disclaimer:</b> The data displayed on this page is sourced directly from the <b>Official Power Morphicon (PMC) website</b>. We do not verify the accuracy of the information provided by PMC. Therefore, this service will not be responsible for any errors, inaccuracies, or discrepancies in the data. The information is provided "as is," and we recommend verifying details directly with PMC for the most accurate and up-to-date information.</p>
         <p className='copyright'>Copyright &copy; {(new Date().getFullYear())} the13thgeek&trade;. All rights reserved.</p>
         <p>v0.5.1</p>
     </footer>
