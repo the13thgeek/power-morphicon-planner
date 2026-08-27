0f14f9cd37775466e1036802fa6c265f577e4f00
the13thgeek <aldreich.aguilar@yahoo.com>
2026-08-27 19:56:04 -0400

    PMC Day Zero update

--- src/components/UpdateText.jsx
+++ src/components/UpdateText.jsx
@@ -11,7 +11,7 @@ const UpdateText = () => {
             <h3>Latest Updates</h3>
             {/* <p>This page is currently being updated for the <b>Power Morphicon 2026</b>. Stay tuned for new information coming soon!</p> */}
             <ul>
-              <li>Aug 27, 2026 - Updated <b>Photo Ops</b> and <b>Panels</b> information</li>
+              <li>Aug 27, 2026 - Updated <b>Photo Ops</b> and <b>Panels</b> information; minor UI improvements</li>
               <li>Aug 26, 2026 - Updated <b>Guests</b> and <b>Panels</b> information</li>
               <li>Aug 25, 2026 - Updated <b>Guests</b>, <b>Photo Ops</b> and <b>Panels</b> information; fixed <b>Photo Ops</b> sorting bug</li>
             </ul>
